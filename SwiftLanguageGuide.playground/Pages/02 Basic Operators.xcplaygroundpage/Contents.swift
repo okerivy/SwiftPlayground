27c8b5a26f1bef748234e02aa12c339fbee039f0
okerivy <okerivy@163.com>
2018-09-05 13:48:26 +0800

    初始化

---
+++ SwiftLanguageGuide.playground/Pages/02 Basic Operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous) | [Next](@next)
+/*:
+ # 01 THE BASICS
+ ****
+ 
+ 第一章,介绍基本操作
+ 
+ ****
+ */
+
+
+import Foundation
+
+var str = "Hello, playground"
+
+
+
+
+//: [Previous](@previous) | [Next](@next)
